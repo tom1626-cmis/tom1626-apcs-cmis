afefa87123d71b67e25b25fd263cc1b22844fca9
cmis <cmis@cmis-hs19.(none)>
2015-08-18 09:55:21 +0700

    add fundamentalsp1

---
+++ FundamentalsP1/FundamentalsP1.java
@@ -0,0 +1,19 @@
+
+public class FundamentalsP1{
+    public static void main (String[] args)
+    { String food, movie, color, group;
+      Scanner scanner = new scanner(System.in);
+      
+      System.out.print("Favorite color:");
+      color = scanner1.nextLine();
+      System.out.print("Favorite food:");
+      food = scanner1.nextLine();
+      System.out.print("Favorite musical group:");
+      group = scanner1.nextLine();
+      System.out.print("Favorite movie:");
+      movie = scanner1.nextLine();
+      
+      
+
+
+       
