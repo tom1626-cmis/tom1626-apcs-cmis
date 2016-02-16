17f1a381ead82883c0f50181666615ce3610b24d
tom1626-cmis <tom1626@cmis.ac.th>
2016-02-16 09:05:53 +0700

    2d7

--- Array2D7/Game2.java
+++ Array2D7/Game2.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-public class Game extends JPanel implements ActionListener
+public class Game2 extends JPanel implements ActionListener
 {
     int[][] numbers = new int[10][10];
     JLabel[] Labels = new JLabel[100];
@@ -12,10 +12,10 @@ public class Game extends JPanel implements ActionListener
     JTextField P1Turn = new JTextField(5); 
     JTextField P2Turn = new JTextField(5); 
     boolean turn = true;
-    public Game()
+    public Game2()
     {
         super(new BorderLayout());
-        // filling in numbers
+        //first round
         for(int row = 0; row< numbers.length; row++)
         {
             for(int col = 0; col< numbers[0].length; col++)
@@ -23,21 +23,20 @@ public class Game extends JPanel implements ActionListener
                 numbers[row][col] = (int)(Math.random()*99 +1);
             }
         }
-        // setting answer
+        // setting answer first round
         int rowAnswer = (int)(Math.random()*9);
         int colAnswer = (int)(Math.random()*9);
         numbers[rowAnswer][colAnswer] = 0;
         // setting up interface tools
-        P1Turn.setEnabled(false);
-        P2Turn.setEnabled(false);
+        P1Turn.setEnabled(false);P1Turn.setDisabledTextColor(Color.BLACK);
+        P2Turn.setEnabled(false);P2Turn.setDisabledTextColor(Color.BLACK);
         for(int i= 1; i <= 100; i++)
         {
             Labels[i-1]  = new JLabel(String.valueOf(i)); 
             Buttons[i-1] = new JButton(String.valueOf(i));Buttons[i-1].setSize(new Dimension(5,5));
             Buttons[i-1].addActionListener(this);
-            Texts[i-1] = new JTextField(5); Texts[i-1].setEnabled(false);
+            Texts[i-1] = new JTextField(5); Texts[i-1].setEnabled(false);Texts[i-1].setDisabledTextColor(Color.BLACK);
         }
-        //
         JPanel LabelText = new JPanel(new GridLayout(20,10));
         for(int i = 0;i < 10;i++)
         {
@@ -76,6 +75,36 @@ public class Game extends JPanel implements ActionListener
         add(LabelText,BorderLayout.PAGE_START);
         add(ButtonPanel,BorderLayout.CENTER);
         add(PlayerPanel,BorderLayout.LINE_START);
+        //GAME
+        JButton Reset = new JButton("Reset");
+        // filling in numbers
+        Reset.addActionListener(new ActionListener()
+            {
+                public void actionPerformed(ActionEvent e)
+                {
+                    for( int i = 0; i < 100; i ++)
+                    {
+                        Buttons[i].setEnabled(true);
+                        Texts[i].setText("");
+                    }
+                    P1Turn.setText("Turn");
+                    P2Turn.setText("");
+                    turn = true;
+                    for(int row = 0; row< numbers.length; row++)
+                    {
+                        for(int col = 0; col< numbers[0].length; col++)
+                        {
+                            numbers[row][col] = (int)(Math.random()*99 +1);
+                        }
+                    }
+                    // setting answer
+                    int rowAnswer = (int)(Math.random()*9);
+                    int colAnswer = (int)(Math.random()*9);
+                    numbers[rowAnswer][colAnswer] = 0;
+
+                }
+            });
+        add(Reset,BorderLayout.LINE_END);
     }
 
     public void actionPerformed(ActionEvent e) 
@@ -123,7 +152,7 @@ public class Game extends JPanel implements ActionListener
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         //Create and set up the content pane.
-        JComponent newContentPane = new Game();
+        JComponent newContentPane = new Game2();
         newContentPane.setOpaque(true); //content panes must be opaque
         frame.setContentPane(newContentPane);
 
