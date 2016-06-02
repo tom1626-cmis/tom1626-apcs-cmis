87d66ddb3695e8a374c267f8cd816f3a018df3b5
tom1626-cmis <tom1626@cmis.ac.th>
2016-06-02 18:07:27 +0700

    slight change

--- SuperSmashBros/Effects.java
+++ SuperSmashBros/Effects.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Effects extends Actor
+public abstract class Effects extends Actor
 {
     int timer;
     public Effects(int n)
